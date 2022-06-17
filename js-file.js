020be10ab433cf48f307eed16cbe2c448e81531c
Chris <chris.p.bradford@gmail.com>
2022-06-17 15:46:05 -0500

    General update following the lesson

--- js-file.js
+++ js-file.js
@@ -22,3 +22,15 @@ container.appendChild(h1);
 container.appendChild(div);
 
 
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
+
